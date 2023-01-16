7c583000d64d953e083a60a9a5dca4a281d6e1df
aahashwani <110942485+aahashwani@users.noreply.github.com>
2023-01-15 21:03:15 -0600

    changes2

--- controllers/thought-ontroller.js
+++ controllers/thought-ontroller.js
@@ -1,1 +1,6 @@
 const { Users, Thoughts } = require("../models");
+
+module.exports = 
+// get all thought models
+getThoughtMod (users = undefined)
+Thoughts.find({}, function (err,)   { return err })
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,13 @@
 const { Users, Thoughts } = require("../models");
 
+const createUser = (req, res) => { return new User(req, res || {}, req. authenticated = false); };
+
+module.exports = 
+
+getUser(req, res) 
+    Users.find()
+
+const userControllers = {
+    createUser: ({body}, callback)
+}
+
